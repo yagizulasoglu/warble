677149e3c21bcca9cfe6351c252eb2c73f640349
David Sapiro <dsap48@gmail.com>
2024-03-07 12:33:50 -0800

    add some tests

--- test_user_model.py
+++ test_user_model.py
@@ -30,7 +30,11 @@ db.create_all()
 
 
 class UserModelTestCase(TestCase):
+    """Model to test our models"""
+
     def setUp(self):
+        """Set up that runs before every test"""
+
         User.query.delete()
 
         u1 = User.signup("u1", "u1@email.com", "password", None)
@@ -41,11 +45,86 @@ class UserModelTestCase(TestCase):
         self.u2_id = u2.id
 
     def tearDown(self):
+        """Tear down that runs after every test"""
+
         db.session.rollback()
 
     def test_user_model(self):
+        """Tests that users are created correctly"""
+
         u1 = User.query.get(self.u1_id)
 
         # User should have no messages & no followers
         self.assertEqual(len(u1.messages), 0)
         self.assertEqual(len(u1.followers), 0)
+
+    def test_is_following(self):
+        """Tests that User method is_following accurately detects if
+        a user is following another user"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        u1.following.append(u2)
+        db.session.commit()
+
+        result = u1.is_following(u2)
+        self.assertTrue(result)
+
+        result2 = u2.is_following(u1)
+        self.assertFalse(result2)
+
+    def test_is_followed_by(self):
+        """Tests that User method is_followed_by fucntions correctly"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        u1.following.append(u2)
+        db.session.commit()
+
+        result = u2.is_followed_by(u1)
+        self.assertTrue(result)
+
+        result2 = u1.is_followed_by(u2)
+        self.assertFalse(result2)
+
+    def test_signup_success(self):
+        """Tests user.signup adds successfully with valid credentials"""
+
+        user = User.signup(
+            username="test_user",
+            email="test@email.com",
+            password="password"
+        )
+
+        db.session.commit()
+
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.username, "test_user")
+        self.assertNotEqual(user.password, "password")
+
+    def test_signup_fail(self):
+
+        user = User.signup(
+            email="test@email.com",
+            password="password"
+        )
+
+        self.assertFalse(user)
+        self.assertNotIsInstance(user, User)
+
+        #TODO: figure out how to assert that an integrity error is raised.
+
+
+
+
+
+
+
+
+
+
+
+
+
